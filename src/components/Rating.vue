56ff462ec072690786effdf5cdc775d57fa2be90
Vlada Bobina <vlada.bobina@t-systems.ru>
2018-01-18 18:54:27 +0300

    Vue project structure

---
+++ src/components/Rating.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="rating">
+  <ul class="list">
+    <li v-for="star in maxStars" :class="{ 'active': star <= stars}" class="star">
+      <icon name="star <= stars ? 'star' : 'star-o'"/>
+    </li>
+  </ul>
+  <span>3 of 5</span>
+  </div>
+</template>
+
+<script>
+  import 'vue-awesome/icons/star'
+  import 'vue-awesome/icons/star-o'
+  import Icon from 'vue-awesome/components/Icon'
+
+  export default {
+    components: { Icon },
+    data() {
+      return {
+        stars: 3,
+        maxStars: 5
+      }
+    }
+  }
+
+</script>
+
+<style lang="scss" scoped>
+  .rating {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    font-size: 14px;
+    color: #a7a8a8;
+
+  }
+  .list {
+    margin: 0 0 5px 0;
+    padding: 0;
+    list-style-type: none;
+  }
+  .list:hover .star {
+    color: #f3d23e;
+  }
+  .star {
+    display: inline-block;
+    cursor: pointer;
+  }
+  .star:hover ~ .star:not(.active) {
+    color: inherit;
+  }
+  .active {
+    color: #f3d23e;
+  }
+</style>
